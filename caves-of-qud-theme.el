228c207e700660b64691b308ae72e8c598d2c2f2
Emily Hyland <hello@duien.com>
2023-03-11 18:00:07 -0800

    Add autoloads

--- caves-of-qud-theme.el
+++ caves-of-qud-theme.el
@@ -129,3 +129,12 @@
   ))
 
 ;; (setq header-line-format "header")
+
+;;;###autoload
+(and load-file-name
+     (boundp 'custom-theme-load-path)
+     (add-to-list 'custom-theme-load-path
+                  (file-name-as-directory
+                   (file-name-directory load-file-name))))
+
+(provide-theme 'caves-of-qud)
